37ee4c08f770abffbac9f6fd1fa0c0dffe3dedcf
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2020-04-12 16:51:35 +0200

    add generator for generating migration file

---
+++ lib/generators/templates/migration.rb
@@ -0,0 +1,12 @@
+class CreateShortenedUrls < ActiveRecord::Migration[6.0]
+  def change
+    create_table :url_shortener_shortened_urls do |t|
+      t.text :full_link, null: false, length: 2083
+      t.string :shortened_link, unique: true
+
+      t.timestamps
+    end
+    
+    add_index :url_shortener_shortened_urls, :full_link, length: 2083
+  end
+end
