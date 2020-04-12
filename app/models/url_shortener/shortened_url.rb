323af95ba2f7a59350c42188a189059fecbec457
ahmadabdelhalim <ahmadabdelhalimmoustafa@gmail.com>
2020-04-12 17:07:10 +0200

    add the ability to find the original link from the shortened link

--- app/models/url_shortener/shortened_url.rb
+++ app/models/url_shortener/shortened_url.rb
@@ -3,6 +3,14 @@ class UrlShortener::ShortenedUrl < ActiveRecord::Base
   after_create :create_shortened_link
   after_validation :smart_add_url_protocol
 
+  def self.find_link(shortened_link:)
+    if shortened_link_is_valid?(shortened_link)
+      return recover_link(shortened_link)
+    else
+      return nil
+    end
+  end
+
   private
   # Total: 26 + 26 + 10 = DICT_SIZE chars
   CHAR_TO_NUMBER_DICT = [*('a'..'z'), *('A'..'Z'), *('0'..'9')]
@@ -32,5 +40,25 @@ class UrlShortener::ShortenedUrl < ActiveRecord::Base
     unless self.full_link and (self.full_link[/\Ahttp:\/\//] || self.full_link[/\Ahttps:\/\//])
       self.full_link = "http://#{self.full_link}"
     end
+  end
+
+  def self.find_shortened_link_primary_id(shortened_link)
+    primary_id = 0
+    shortened_link.reverse!
+    shortened_link.each_char.with_index do |char, i|
+      primary_id += CHAR_TO_NUMBER_DICT[char] * (DICT_SIZE ** i)
+    end
+    primary_id
+  end
+  
+  def self.recover_link(shortened_link)
+    link_id = find_shortened_link_primary_id(shortened_link)
+    self.find_by(id: link_id)
+  end
+  
+  # Shortened link should not exceed 6 characters, nor includes illegal characters
+  # Otherwise calculating primary id would result into invalid number
+  def self.shortened_link_is_valid?(shortened_link)
+    !!(shortened_link =~ /^[A-Za-z0-9]{1,6}$/)
   end
 end
